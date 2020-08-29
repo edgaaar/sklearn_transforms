fe1771d7364dc9e29435a2321ed505169589622d
Edgar Saldaña <edg.7007@gmail.com>
2020-08-29 11:16:26 -0500

    Agrego transformaciones

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -15,11 +15,58 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-class Sampler():
+class OverSample(BaseEstimator, TransformerMixin):
+    def __init__(self, column):
+        self.column = column
+        
+    def over_excelent(self, dataset, col):
+        intermed = dataset[dataset[col]=='EXCELENTE']
+        dataset = pd.concat([dataset, intermed])
+        return dataset
+    
+    def over_good(self, dataset, col):
+        intermed = dataset[dataset[col]=='MUITO_BOM']
+        dataset = pd.concat([dataset, intermed])
+        return dataset
+    
+    def fit(self,  X, y=None, **fit_params):
+        return self
+    
+    def transform(self, X, **transform_params):
+        X = self.over_good(X, self.column)
+        dataset = self.over_excelent(X, self.column) 
+        return dataset
+
+class DropColumns2(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
 
     def fit(self, X, y=None):
         return self
-    
-    def transform(self, X, y):
-        X_rus, y_rus = rus.fit_sample(features_df, target_df)
-        return pd.DataFrame(X_rus), y_rus
+
+    def transform(self, X):
+        data = X.copy()
+        
+        print(data)
+        
+        sos = data['OBJETIVO'] == 'Sospechoso'
+        sos2 = data[sos]
+        data0 = sos2[0:-1]
+
+        sos3 = data['OBJETIVO'] == 'Sospechoso'
+        sos4 = data[sos3]
+        data1 = sos4[0:-1]
+        
+        sos5 = data['OBJETIVO'] == 'Sospechoso'
+        sos6 = data[sos5]
+        data2 = sos6[0:-1]
+
+        ss = pd.concat([data, data0])
+        sss = pd.concat([ss, data1])
+        ssss = pd.concat([sss,data2])
+
+        data = ssss
+        data = shuffle(data)
+        data
+        
+        return data.drop(labels=self.columns, axis='columns')
